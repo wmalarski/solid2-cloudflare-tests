510e3827a7521738be990c7244dde035b97ee16e
Wojtek Malarski <w.malarski@gmail.com>
2026-06-11 20:56:49 +0200

    Initial commit from Vite+

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite-plus";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  plugins: [solid()],
+});
