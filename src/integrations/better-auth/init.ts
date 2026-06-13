bb687fe3f7e4b61f2362394d0487e3f7c107f81c
Wojtek Malarski <w.malarski@gmail.com>
2026-06-13 10:12:09 +0200

    adds spotify provider

--- src/integrations/better-auth/init.ts
+++ src/integrations/better-auth/init.ts
@@ -13,6 +13,12 @@ export const initBetterAuth = ({ db, env }: InitBetterAuthArgs) => {
     baseURL: env.BETTER_AUTH_URL,
     database: drizzleAdapter(db, { provider: "sqlite", schema }),
     secret: env.BETTER_AUTH_SECRET,
+    socialProviders: {
+      spotify: {
+        clientId: env.SPOTIFY_CLIENT_ID,
+        clientSecret: env.SPOTIFY_CLIENT_SECRET,
+      },
+    },
   });
 };
 
